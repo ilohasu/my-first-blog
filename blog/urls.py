d0f25a9c6dffb38ab3c5ca9d728e1ee8b085c046
irohasu <test@test.com>
2020-07-05 19:39:28 +0900

    blogサイトの完成

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,12 @@ app_name = 'blog'
 
 urlpatterns = [
     #path('', views.post_list, name='post_list'),
-    path('', generic.TemplateView.as_view(template_name='blog/test.html'), name='test'),
+    #path('', generic.TemplateView.as_view(template_name='blog/test.html'), name='test'),
+    path('', views.DiaryList.as_view(), name='list'),
+    path('detail/<int:pk>/', views.DiaryDetail.as_view(), name='detail'),
+    path('category/<int:pk>/', views.DiaryCategoryList.as_view(), name='category'),
+    path('archive/<int:year>/', views.DiaryYearList.as_view(), name='year'),
+    path('archive/<int:year>/<int:month>/', views.DiaryMonthList.as_view(), name='month'),
+    path('search/', views.DiarySearchList.as_view(), name='search'),
 ]
 
--- readme.txt
+++ readme.txt
@@ -33,6 +33,20 @@ git commit -m "[コミットメッセージ]"
 git remote add origin https://github.com/ilohasu/my-first-blog.git
 git push -u origin master
 
+$ git status
+$ git add --all .
+$ git status
+$ git commit -m "Added view and template for detailed blog post as well as CSS for the site."
+$ git push
+
+# サーバへの反映
+cd ~/ilohasu.pythonanywhere.com
+git pull
+
+# 静的ファイルの更新
+workon ilohasu.pythonanywhere.com
+(ola.pythonanywhere.com)$ python manage.py collectstatic
+
 9183e93b4f8d6fe0564fa7c8df896a598d6d6126
 -------------------------------
 
